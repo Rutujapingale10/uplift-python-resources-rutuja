b6817cd950fa2226ebb671e94a77ff3c78a48245
Rutujapingale10 <rutujapingale729@gmail.com>
2021-07-03 12:16:21 +0100

    smallest_number_in_list

---
+++ list/smallest_number.py
@@ -0,0 +1,12 @@
+my_list = [67,50,4.4,56,32,12,99]
+my_list.sort()
+# for smallest
+print(my_list[0])
+# for largest 
+print(my_list[-1])
+# OR 
+# largest number
+my_list.sort(reverse=True)
+print(my_list[0])
+#second largest no
+print(my_list[1])
